132ff40c5cf03333bcae4ad2c8ef00ad879b8ab6
Ángela García <97111035+garciahuelamo@users.noreply.github.com>
2023-05-09 20:07:07 +0200

    Add files via upload

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"datos","l":"BD"},{"p":"control","l":"Gestion"},{"p":"usuario","l":"SolicitudDatos"},{"p":"control","l":"Transformacion"},{"p":"control","l":"Utilidades"},{"p":"control","l":"Videojuegos"}];updateSearchResults();
